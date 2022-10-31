d1682570ceb984f827db702a80e556677d565683
Michel Sommer <skruen@gmail.com>
2022-10-31 22:31:37 +0100

    add session 8

--- Session 8/i2cTest/main.cpp
+++ Session 8/i2cTest/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <unistd.h>
+#include <pthread.h>
+#include "HIH8120.h"
+//#include "BMP180.h"
+#include <stdio.h>
+#include <iomanip>
 
-int main() {
+using namespace std;
+using namespace exploringBB;
+
+int main(int argc, char *argv[]) {
+    unsigned int i2c_bus = 2;
+
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
